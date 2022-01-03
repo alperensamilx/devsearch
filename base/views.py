5cf218bda469336100b88ccfd5f282ec5a8bdc45
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-03 17:41:10 +0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def project(request):
+    return render(request, 'simple-project.html')
+
