69aa45b6bf191d552f52e7a6e56edbb44600ce89
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-03 19:30:38 +0300

    first commit

--- base/views.py
+++ base/views.py
@@ -2,11 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 
 def projects(request):
-    return render(request, 'projects.html')
+    context = {'projects': projectsList}
+    return render(request, 'base/projects.html', context)
 
 
-def project(request):
-    return render(request, 'simple-project.html')
+def project(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'base/single-project.html', {'project': projectObj})
 
