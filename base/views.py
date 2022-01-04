77a0576585b812cd574f25e6006a43bf9286ae25
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-04 14:18:35 +0300

    database changes v2 commit

--- base/views.py
+++ base/views.py
@@ -1,35 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
-
 
 def projects(request):
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'base/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
+
     return render(request, 'base/single-project.html', {'project': projectObj})
 
