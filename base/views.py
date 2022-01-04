efa786604febd72969053b94818f4073a79bda24
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-04 17:55:51 +0300

    CRUD added commit

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
+
 
 # Create your views here.
 
@@ -14,4 +16,31 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
 
     return render(request, 'base/single-project.html', {'project': projectObj})
+
+
+def create_project(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'base/project_form.html', context)
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'base/project_form.html', context)
 
