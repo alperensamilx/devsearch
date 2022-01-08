723b42c25d89231776cc9eb4a58aa4a44e019a9a
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-08 23:21:40 +0300

    paginator updated commit

--- base/utlis.py
+++ base/utlis.py
@@ -1,5 +1,36 @@
 from .models import *
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def pagination_projects(request, projects, results):
+
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
+
+    left_index = (int(page) - 1)
+
+    if left_index < 1:
+        left_index = 1
+
+    right_index = (int(page) + 2)
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+    custom_range = range(left_index, right_index)
+
+    return custom_range, projects
 
 
 def search_projects(request):
--- base/views.py
+++ base/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .forms import *
 from .utlis import search_projects
 
@@ -11,20 +10,9 @@ from .utlis import search_projects
 def projects(request):
     projects, search_query = search_projects(request)
 
-    page = request.GET.get('page')
-    results = 3
-    paginator = Paginator(projects, results)
 
-    try:
-        projects = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1
-        projects = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        projects = paginator.page(page)
 
-    context = {'projects': projects, 'search_query': search_query, 'paginator': paginator}
+    context = {'projects': projects, 'search_query': search_query, 'paginator': paginator, 'custom_range': custom_range}
     return render(request, 'base/projects.html', context)
 
 
