c8ffe1d7e2c49e10a22cd09a78b682fdf9ed5717
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-09 15:04:19 +0300

    email send commit

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,6 @@
 from django.db.models.signals import post_save, post_delete
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import *
 
 
@@ -7,6 +9,18 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user = instance
         profile = Profile.objects.create(user=user, username=user.username, email=user.email, name=user.first_name)
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+
+        )
 
 
 def update_user(sender, instance, created, **kwargs):
