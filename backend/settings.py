cf322bf453859304a73ad42f67305065923302eb
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-09 22:49:02 +0300

    rest framework commit

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,16 @@ INSTALLED_APPS = [
 
     'base.apps.BaseConfig',
     'users.apps.UsersConfig',
+
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/urls.py
+++ backend/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('base.urls')),
     path('', include('users.urls')),
+    path('api/', include('api.urls')),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name="password_reset_done"),
@@ -32,5 +33,5 @@ urlpatterns = [
     path('reset_password_compate/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), name="password_reset_complete"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
